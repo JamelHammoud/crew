c824ebd960eb8b6f3d5c38c2d5faa606013b783b
Jamel Hammoud <devjamel@gmail.com>
2026-07-19 21:33:57 -0700

    crew sync

--- src/renderer/src/components/TopBar.tsx
+++ src/renderer/src/components/TopBar.tsx
@@ -2,6 +2,7 @@ import { ArrowRightStartOnRectangleIcon, CheckIcon, LinkIcon } from '@heroicons/
 import { useState } from 'react'
 import { useCrew } from '../state/store'
 import Avatar from './Avatar'
+import TabIcon from './TabIcon'
 import { MenuItem, Popover } from './Popover'
 
 export type Tab = 'chat' | 'agents' | 'docs'
