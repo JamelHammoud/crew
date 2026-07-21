17e5567d903d3b03a2103f9246a71ba94b34d3c4
Ali <alihammoud0202@gmail.com>
2026-07-21 12:41:34 -0300

    crew sync

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { useCrew } from './state/store'
 import { applyTheme, storedTheme } from './state/theme'
 import './styles.css'
 
 applyTheme(storedTheme())
+void useCrew.getState().boot()
 
 const root = document.getElementById('root')!
 window.crew.onFullScreen(full => root.classList.toggle('square', full))
