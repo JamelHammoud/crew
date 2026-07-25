9f5982783b031f9eab97326dc54c52d0baf1f5d6
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 16:51:42 -0700

    crew sync

--- src/renderer/src/views/Design.tsx
+++ src/renderer/src/views/Design.tsx
@@ -44,7 +44,7 @@ export default function Design() {
     <DesignBoardContext.Provider value={boardContext}>
       <EditorContext.Provider value={editor}>
         <div className="h-full flex flex-col" style={{ paddingTop: TOP_BAR_H }}>
-          <div className="app-drag h-11 shrink-0 flex items-center gap-1 px-6">
+          <div className="app-drag h-10 shrink-0 flex items-center gap-1 px-6">
             <HeaderButton
               label={leftOpen ? 'Hide layers' : 'Show layers'}
               pressed={leftOpen}
