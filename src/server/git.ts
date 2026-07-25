f4832c77c83976272d3b10b6ff5b99e268bf1ac9
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 14:27:24 -0700

    crew sync

--- src/server/git.ts
+++ src/server/git.ts
@@ -8,7 +8,7 @@ import {
   takeSyncLock,
   type GitResult
 } from '../shared/git'
-import { interruptedStates, rebaseActive } from '../shared/gitState'
+import { interruptedStates } from '../shared/gitState'
 import type { RepoActionResult, RepoChange, RepoChangeKind, RepoStatus } from '../shared/repository'
 
 const PROJECT_PATHS = ['.', ':(exclude).crew', ':(exclude).crew/**']
@@ -326,10 +326,6 @@ export class GitSync {
       }
     }
     return kept
-  }
-
-  private rebaseActive(): Promise<boolean> {
-    return rebaseActive(this.repoPath)
   }
 
   private async readStatus(): Promise<RepoStatus> {
