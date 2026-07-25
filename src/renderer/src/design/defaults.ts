8acd05fbeae3250c4fc7ac0dc7d835ade90c01d9
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 16:46:43 -0700

    crew sync

--- src/renderer/src/design/defaults.ts
+++ src/renderer/src/design/defaults.ts
@@ -1,21 +1,7 @@
-import {
-  DefaultDashStyle,
-  DefaultFontStyle,
-  DefaultHorizontalAlignStyle,
-  DefaultTextAlignStyle,
-  LineShapeSplineStyle,
-  type Editor
-} from 'tldraw'
+import { DefaultDashStyle, DefaultFontStyle, LineShapeSplineStyle, type Editor } from 'tldraw'
 
 export function applyDesignDefaults(editor: Editor): void {
-  editor.run(
-    () => {
-      editor.setStyleForNextShapes(DefaultFontStyle, 'sans')
-      editor.setStyleForNextShapes(DefaultDashStyle, 'solid')
-      editor.setStyleForNextShapes(LineShapeSplineStyle, 'line')
-      editor.setStyleForNextShapes(DefaultTextAlignStyle, 'start')
-      editor.setStyleForNextShapes(DefaultHorizontalAlignStyle, 'start')
-    },
-    { history: 'ignore' }
-  )
+  editor.setStyleForNextShapes(DefaultFontStyle, 'sans')
+  editor.setStyleForNextShapes(DefaultDashStyle, 'solid')
+  editor.setStyleForNextShapes(LineShapeSplineStyle, 'line')
 }
