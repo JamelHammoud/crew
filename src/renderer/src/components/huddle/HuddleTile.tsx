2d2e95c06b4bb44873b01eda2b5cae7f32a58f46
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 15:57:25 -0700

    crew sync

--- src/renderer/src/components/huddle/HuddleTile.tsx
+++ src/renderer/src/components/huddle/HuddleTile.tsx
@@ -119,9 +119,7 @@ export default function HuddleTile({
 
       <InsetRing
         className={`transition-all duration-200 ${
-          speaking
-            ? 'border-2 border-fg shadow-[inset_0_0_24px_-4px_rgb(255_255_255/0.45)]'
-            : 'border border-fg/[0.07]'
+          speaking ? `border-2 border-fg ${look.bloom}` : 'border border-fg/[0.07]'
         }`}
       />
     </div>
