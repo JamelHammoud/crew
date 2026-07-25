f600465a9ba2f87bac81cad93a4fdd09eddbac7f
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 16:48:14 -0700

    crew sync

--- src/renderer/src/design/defaults.ts
+++ src/renderer/src/design/defaults.ts
@@ -1,7 +1,8 @@
 import { DefaultDashStyle, DefaultFontStyle, LineShapeSplineStyle, type Editor } from 'tldraw'
+import { DESIGN_STYLE_DEFAULTS } from '../../../shared/design'
 
 export function applyDesignDefaults(editor: Editor): void {
-  editor.setStyleForNextShapes(DefaultFontStyle, 'sans')
-  editor.setStyleForNextShapes(DefaultDashStyle, 'solid')
-  editor.setStyleForNextShapes(LineShapeSplineStyle, 'line')
+  editor.setStyleForNextShapes(DefaultFontStyle, DESIGN_STYLE_DEFAULTS.font)
+  editor.setStyleForNextShapes(DefaultDashStyle, DESIGN_STYLE_DEFAULTS.dash)
+  editor.setStyleForNextShapes(LineShapeSplineStyle, DESIGN_STYLE_DEFAULTS.spline)
 }
