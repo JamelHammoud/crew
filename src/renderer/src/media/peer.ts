57810d8985000a6ebb8e25088d105dc911559ce2
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 16:47:14 -0700

    crew sync

--- src/renderer/src/media/peer.ts
+++ src/renderer/src/media/peer.ts
@@ -68,7 +68,7 @@ export class PeerLink {
     this.pc = new RTCPeerConnection({ iceServers: ICE_SERVERS, bundlePolicy: 'max-bundle' })
 
     this.remote = { mic: new MediaStream(), camera: new MediaStream(), screen: new MediaStream() }
-    if (true) {
+    if (this.caller) {
       this.bind([
         this.pc.addTransceiver('audio', { direction: 'sendrecv' }),
         this.pc.addTransceiver('video', { direction: 'sendrecv' }),
@@ -77,7 +77,7 @@ export class PeerLink {
     }
 
     this.pc.onnegotiationneeded = () => {
-      void this.offer()
+      if (this.caller) void this.offer()
     }
     this.pc.onicecandidate = ({ candidate }) => {
       if (candidate) this.send({ kind: 'candidate', candidate: candidate.toJSON() })
