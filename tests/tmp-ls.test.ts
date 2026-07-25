9a3e5f47b287a043900a0c25242644aee4e0bee9
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 14:26:08 -0700

    crew sync

--- tests/tmp-ls.test.ts
+++
@@ -1,6 +0,0 @@
-// @vitest-environment jsdom
-import { it, expect } from 'vitest'
-it('has storage', () => {
-  console.log('doc url', document.URL, 'win ls', typeof window.localStorage, 'global ls', typeof localStorage)
-  expect(1).toBe(1)
-})
