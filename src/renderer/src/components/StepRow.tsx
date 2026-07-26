eeb0475ee071eb54041a1a6afe3bc415f91d26a1
Jamel Hammoud <devjamel@gmail.com>
2026-07-25 23:10:24 -0700

    crew sync

--- src/renderer/src/components/StepRow.tsx
+++ src/renderer/src/components/StepRow.tsx
@@ -14,6 +14,7 @@ import {
   TextWithFileLinks,
   useLocated
 } from './fileLinks'
+import Markdown from './Markdown'
 import StepCode from './StepCode'
 import StepDiff from './StepDiff'
 import type { ThreadItem } from './thread'
