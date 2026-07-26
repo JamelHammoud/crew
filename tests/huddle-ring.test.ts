c3cfef92da8de9b21141246fe1e4813eddf9da34
Jamel Hammoud <devjamel@gmail.com>
2026-07-26 12:03:02 -0700

    crew sync

--- tests/huddle-ring.test.ts
+++ tests/huddle-ring.test.ts
@@ -139,7 +139,7 @@ const peer = (peerId: string, name: string): HuddlePeer => ({
   joinedAt: 1
 })
 
-const room = (...peers: HuddlePeer[]): HuddleRoom => ({ peers, startedAt: 10 })
+const room = (...peers: HuddlePeer[]): HuddleRoom => ({ id: 'call-1', peers, startedAt: 10 })
 const arrives = (next: HuddleRoom): void => wire.deliver?.({ type: 'huddle.room', room: next })
 
 const backToHome = (): void => {
