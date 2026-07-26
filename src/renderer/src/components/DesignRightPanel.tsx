0ea45a98fc3dc889da740356f65b3b745f8c7fcd
Jamel Hammoud <devjamel@gmail.com>
2026-07-26 16:20:07 -0700

    crew sync

--- src/renderer/src/components/DesignRightPanel.tsx
+++ src/renderer/src/components/DesignRightPanel.tsx
@@ -1,25 +1,12 @@
-import { useState } from 'react'
-import { PlusGlyph } from '../icons'
-import DesignChat, { useBoardThreads } from './DesignChat'
-import { PanelButton } from './DesignControls'
+import DesignChat from './DesignChat'
 
 export default function DesignRightPanel({ boardId }: { boardId: string }) {
-  const [composeNew, setComposeNew] = useState(false)
-  const boardThreads = useBoardThreads(boardId)
-
   return (
     <aside
       aria-label="Board chat"
       className="w-[340px] shrink-0 flex flex-col min-w-0 min-h-0 overflow-hidden bg-ink-900 border-l border-ink-700"
     >
-      {boardThreads.length > 0 && (
-        <div className="h-12 shrink-0 flex items-center justify-end pr-2">
-          <PanelButton label="New thread" onClick={() => setComposeNew(true)}>
-            <PlusGlyph className="w-4 h-4" />
-          </PanelButton>
-        </div>
-      )}
-      <DesignChat key={boardId} boardId={boardId} composeNew={composeNew} onComposeNew={setComposeNew} />
+      <DesignChat key={boardId} boardId={boardId} />
     </aside>
   )
 }
