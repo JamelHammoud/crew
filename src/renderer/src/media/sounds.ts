b075f7abfa2bd1d54749b888468a881e92afda7a
Jamel Hammoud <devjamel@gmail.com>
2026-07-26 15:33:34 -0700

    crew sync

--- src/renderer/src/media/sounds.ts
+++ src/renderer/src/media/sounds.ts
@@ -5,7 +5,7 @@ import { playStrikes, type Strike } from './strike'
 import { playNotes, type Note } from './tone'
 
 type ChimeName = 'send' | 'receive' | 'done' | 'failed' | 'join' | 'leave'
-type StrikeName = 'tab.chat' | 'tab.docs' | 'tab.design'
+type StrikeName = 'tab.chat' | 'tab.docs' | 'tab.design' | 'sound.on'
 
 export type SoundName = ChimeName | StrikeName
 
