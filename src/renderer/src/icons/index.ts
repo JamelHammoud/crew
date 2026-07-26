006dec43e6b06b54d58c83f4c7f8a6b29f64a540
Jamel Hammoud <devjamel@gmail.com>
2026-07-26 11:57:07 -0700

    crew sync

---
+++ src/renderer/src/icons/index.ts
@@ -0,0 +1,9 @@
+// One drawing per idea, in one place. Anything that needs an icon reaches in
+// here, and a concept that is already drawn is never drawn a second time.
+export type { Glyph } from '../components/glyph'
+export * from './keylines'
+export * from './navigation'
+export * from './actions'
+export * from './objects'
+export * from './status'
+export * from './media'
