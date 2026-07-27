0ef3887f65fb9319700c2d608e8c02d9b5953edd
Jamel Hammoud <devjamel@gmail.com>
2026-07-26 17:08:41 -0700

    crew sync

--- scripts/.dbg-entry.ts
+++
@@ -1,5 +0,0 @@
-
-import { musicItems } from '../src/shared/music'
-import { coverArt } from '../src/renderer/src/components/music/coverSeed'
-import { coverFor } from '../src/renderer/src/components/music/coverArt'
-window.D = { musicItems, coverArt, coverFor }
