a1dd805850972d4da1083d50b3547c2259da76e5
Jamel Hammoud <devjamel@gmail.com>
2026-07-27 22:30:06 -0700

    crew sync

--- src/renderer/src/components/GhostBar.tsx
+++ src/renderer/src/components/GhostBar.tsx
@@ -8,7 +8,7 @@ import { TOP_BAR_H } from './TopBar'
 // glass, because one is something to press and this is where you are.
 export default function GhostBar() {
   return (
-    <div style={{ top: TOP_BAR_H }} className="absolute left-1/2 -translate-x-1/2 z-30 animate-pop">
+    <div style={{ top: TOP_BAR_H }} className="absolute left-1/2 -translate-x-1/2 z-[45] pt-2 animate-pop">
       <Tooltip label="Only you can see this, and none of it is written down">
         <span className="flex items-center gap-1.5 h-9 pl-3 pr-4 rounded-full bg-fg text-ink-900 text-sm font-semibold shadow-[0_10px_30px_rgb(0_0_0/0.35)] cursor-default">
           <GhostGlyph className="w-4 h-4" />
