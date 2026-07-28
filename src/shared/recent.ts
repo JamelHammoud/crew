6b65cf564e5c3d14bd0b5affc3a419caabeea744
Jamel Hammoud <devjamel@gmail.com>
2026-07-27 19:12:09 -0700

    crew sync

--- src/shared/recent.ts
+++ src/shared/recent.ts
@@ -1,6 +1,16 @@
+import type { CrewHome } from './project'
+
 export interface RecentJoin {
   folder: string
   name: string
   link: string
   joinedAt: number
+}
+
+export interface RecentProject {
+  folder: string
+  name: string
+  home: CrewHome
+  key: string
+  openedAt: number
 }
