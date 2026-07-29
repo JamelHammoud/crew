251e529004b1b980b23db802c223b163247dc68b
Jamel Hammoud <devjamel@gmail.com>
2026-07-28 20:59:56 -0700

    crew sync

--- src/renderer/src/components/subagents/SubagentRun.tsx
+++ src/renderer/src/components/subagents/SubagentRun.tsx
@@ -76,7 +76,7 @@ export default function SubagentRun({ threadId }: { threadId: string }) {
 
   return (
     <div className="absolute inset-0 flex flex-col">
-      <div ref={scrollRef} onScroll={onScroll} className="flex-1 min-h-0 overflow-y-auto px-4">
+      <div ref={scrollRef} onScroll={onScroll} className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden px-4">
         <div className="space-y-4 pt-1 pb-4 select-text">
           <ThreadItems items={items} />
           {working && start?.kind === 'agent.start' && (
