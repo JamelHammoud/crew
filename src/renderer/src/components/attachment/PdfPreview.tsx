6d6f2c1be32c53442630b170d57cec42e12cb49d
Jamel Hammoud <devjamel@gmail.com>
2026-07-29 20:02:24 -0700

    crew sync

--- src/renderer/src/components/attachment/PdfPreview.tsx
+++ src/renderer/src/components/attachment/PdfPreview.tsx
@@ -14,6 +14,7 @@ import FindBar from '../FindBar'
 import Skeleton from '../Skeleton'
 import ZoomView from '../ZoomView'
 import { Failed } from './Frame'
+import { pdfAssets } from './pdfAssets'
 
 pdfjs.GlobalWorkerOptions.workerSrc = workerUrl
 
