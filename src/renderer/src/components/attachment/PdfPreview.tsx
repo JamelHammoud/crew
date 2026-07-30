c17a51d8c5edb8e5bcda436630441e4c59011e56
Jamel Hammoud <devjamel@gmail.com>
2026-07-29 19:27:33 -0700

    crew sync

--- src/renderer/src/components/attachment/PdfPreview.tsx
+++ src/renderer/src/components/attachment/PdfPreview.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useLayoutEffect, useRef, useState, type RefObject } from 'react'
-import * as pdfjs from 'pdfjs-dist'
-import workerUrl from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
+import * as pdfjs from 'pdfjs-dist/legacy/build/pdf.mjs'
+import workerUrl from 'pdfjs-dist/legacy/build/pdf.worker.min.mjs?url'
 import type { PDFDocumentLoadingTask, PDFDocumentProxy, RenderTask } from 'pdfjs-dist'
 import Skeleton from '../Skeleton'
 
