2e29989057e13f0e174836f969c03fbf025d1671
Jamel Hammoud <devjamel@gmail.com>
2026-07-29 21:04:13 -0700

    crew sync

--- src/main/index.ts
+++ src/main/index.ts
@@ -67,6 +67,7 @@ const tray = new CrewTray({
   quit: () => app.quit()
 })
 const scribe = new ScribeWindow(rendererPage)
+const awake = new KeepAwake()
 const updates = new Updates(() => appWindows())
 const said = new ScribeHistory()
 let scribeSettings: ScribeSettings = cleanSettings(null, process.platform)
