9e1171bc231958a08ca0349ceb4478060654d7dc
Jamel Hammoud <devjamel@gmail.com>
2026-07-30 22:45:06 -0700

    crew sync

--- scripts/dmg-mesh.js
+++ scripts/dmg-mesh.js
@@ -79,6 +79,7 @@ uniform vec2 uShape;
 uniform float uWide;
 uniform float uFade;
 uniform float uLift;
+uniform float uGrain;
 
 vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
