9b705dfcb5b4f7f2b5e03f6331d40e3fc7c80f13
Jamel Hammoud <devjamel@gmail.com>
2026-07-31 08:05:09 -0700

    crew sync

---
+++ src/renderer/src/canvas/shapes/index.ts
@@ -0,0 +1,37 @@
+export * from './ShapeUtil'
+export * from './DesignNodeUtil'
+export * from './FrameShapeUtil'
+export * from './TextShapeUtil'
+export * from './GeoShapeUtil'
+export * from './ArrowShapeUtil'
+export * from './LineShapeUtil'
+export * from './DrawShapeUtil'
+export * from './HighlightShapeUtil'
+export * from './NoteShapeUtil'
+export * from './ImageShapeUtil'
+export * from './GroupShapeUtil'
+export * from './shared'
+
+import { ArrowBindingUtil, ArrowShapeUtil } from './ArrowShapeUtil'
+import { DrawShapeUtil } from './DrawShapeUtil'
+import { FrameShapeUtil } from './FrameShapeUtil'
+import { GeoShapeUtil } from './GeoShapeUtil'
+import { HighlightShapeUtil } from './HighlightShapeUtil'
+import { ImageShapeUtil } from './ImageShapeUtil'
+import { LineShapeUtil } from './LineShapeUtil'
+import { NoteShapeUtil } from './NoteShapeUtil'
+import { TextShapeUtil } from './TextShapeUtil'
+
+export const defaultShapeUtils = [
+  TextShapeUtil,
+  DrawShapeUtil,
+  GeoShapeUtil,
+  NoteShapeUtil,
+  LineShapeUtil,
+  FrameShapeUtil,
+  ArrowShapeUtil,
+  HighlightShapeUtil,
+  ImageShapeUtil
+] as const
+
+export const defaultBindingUtils = [ArrowBindingUtil] as const
