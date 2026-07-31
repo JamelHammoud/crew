f3e9f7a226b08d4882922ebe17db9adf9a61c05d
Jamel Hammoud <devjamel@gmail.com>
2026-07-30 22:45:19 -0700

    crew sync

--- scripts/dmg-mesh.js
+++ scripts/dmg-mesh.js
@@ -232,6 +232,7 @@ void main() {
     gl.uniform1f(gl.getUniformLocation(program, 'uWide'), BLOOM.wide)
     gl.uniform1f(gl.getUniformLocation(program, 'uFade'), BLOOM.fade)
     gl.uniform1f(gl.getUniformLocation(program, 'uLift'), BLOOM.lift)
+    gl.uniform1f(gl.getUniformLocation(program, 'uGrain'), GRAIN)
     gl.uniform3fv(
       gl.getUniformLocation(program, 'uColour'),
       LAYERS.flatMap(layer => layer.color)
