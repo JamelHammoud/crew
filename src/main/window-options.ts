5cec9d6bc2ce7e4dc6b784168a0c64149147a82c
Jamel Hammoud <devjamel@gmail.com>
2026-07-30 21:43:31 -0700

    crew sync

--- src/main/window-options.ts
+++ src/main/window-options.ts
@@ -66,7 +66,8 @@ export function createScribeOptions(
       preload,
       contextIsolation: true,
       sandbox: false,
-      backgroundThrottling: false
+      backgroundThrottling: false,
+      devTools
     }
   }
 }
