16d15d1ed0ff60faba24f079673679a273ade01c
Jamel Hammoud <devjamel@gmail.com>
2026-07-31 07:46:45 -0700

    crew sync

---
+++ src/renderer/src/canvas/geometry/Point2d.ts
@@ -0,0 +1,25 @@
+import { Vec, type VecLike } from '../math/Vec'
+import { Geometry2d, type Geometry2dOptions } from './Geometry2d'
+
+export class Point2d extends Geometry2d {
+  readonly point: Vec
+
+  constructor(
+    config: Omit<Geometry2dOptions, 'isClosed' | 'isFilled'> & { margin: number; point: Vec }
+  ) {
+    super({ ...config, isClosed: true, isFilled: true })
+    this.point = config.point
+  }
+
+  override getVertices(): Vec[] {
+    return [this.point]
+  }
+
+  override nearestPoint(): Vec {
+    return this.point
+  }
+
+  override hitTestLineSegment(a: VecLike, b: VecLike, margin = 0): boolean {
+    return Vec.DistanceToLineSegment(a, b, this.point) < margin
+  }
+}
