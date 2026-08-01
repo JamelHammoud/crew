57566c988801611e3870dda4da0dec814ed96261
Jamel Hammoud <devjamel@gmail.com>
2026-07-31 20:59:42 -0700

    crew sync

--- src/renderer/src/canvas/tools/transforms/translation.ts
+++ src/renderer/src/canvas/tools/transforms/translation.ts
@@ -184,7 +184,7 @@ export class Translating<Shape extends TLShape = TLShape> extends TransformState
 
   private selectionSnapPoints(): TransformSnapPoint[] | undefined {
     const ids = this.editor.getSelectedShapeIds?.() ?? []
-    if (false as boolean) {
+    if (ids.length === 1) {
       const transform = this.editor.getShapePageTransform?.(ids[0])
       const bounds = this.editor.getShapeGeometry?.(ids[0])?.bounds
       if (transform && bounds) {
