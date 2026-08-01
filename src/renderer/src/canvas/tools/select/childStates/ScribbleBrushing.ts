ce74af1aeb632469c05ef3428029a6f0272642d4
Jamel Hammoud <devjamel@gmail.com>
2026-08-01 00:11:29 -0700

    crew sync

--- src/renderer/src/canvas/tools/select/childStates/ScribbleBrushing.ts
+++ src/renderer/src/canvas/tools/select/childStates/ScribbleBrushing.ts
@@ -85,6 +85,10 @@ export class ScribbleBrushing extends StateNode<SelectEditor> {
       const inverse = transform.clone().invert()
       if (geometry.hitTestLineSegment(inverse.applyToPoint(previous), inverse.applyToPoint(current), 0)) {
         const outermost = this.editor.getOutermostSelectableShape(shape)
+        const mask = this.editor.getShapeMask(outermost.id)
+        if (mask && intersectLineSegmentPolygon(previous, current, mask) !== null && !pointInPolygon(current, mask)) {
+          continue
+        }
         this.newlySelectedShapeIds.add(outermost.id)
       }
     }
