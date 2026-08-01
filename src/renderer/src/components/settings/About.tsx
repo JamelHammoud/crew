01a61ed0900ec2ac33287a7b1c9ad19fbba79c40
Jamel Hammoud <devjamel@gmail.com>
2026-08-01 10:47:23 -0700

    crew sync

--- src/renderer/src/components/settings/About.tsx
+++ src/renderer/src/components/settings/About.tsx
@@ -1,5 +1,6 @@
 import CreditsRow from './CreditsRow'
 import { Page, Section } from './parts'
+import ReportRow from './ReportRow'
 import VersionRow from './VersionRow'
 
 export default function About() {
@@ -8,6 +9,9 @@ export default function About() {
       <Section>
         <VersionRow />
         <CreditsRow />
+      </Section>
+      <Section>
+        <ReportRow />
       </Section>
     </Page>
   )
