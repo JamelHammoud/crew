bfaf95bb0712b96aebfadd9051ca810153a3acde
Jamel Hammoud <devjamel@gmail.com>
2026-07-31 20:30:00 -0700

    crew sync

--- src/renderer/src/components/DesignRightPanel.tsx
+++ src/renderer/src/components/DesignRightPanel.tsx
@@ -1,9 +1,11 @@
+import { BOARD_CHAT_MARK } from '../design/designKeys'
 import DesignChat from './DesignChat'
 
 export default function DesignRightPanel({ boardId }: { boardId: string }) {
   return (
     <aside
       aria-label="Board chat"
+      {...{ [BOARD_CHAT_MARK]: '' }}
       className="w-[340px] shrink-0 flex flex-col min-w-0 min-h-0 overflow-hidden bg-ink-900 border-l border-ink-700"
     >
       <DesignChat key={boardId} boardId={boardId} />
