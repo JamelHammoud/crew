61d54b155b8ac2bbef464117bd78b91efdf3a75a
Jamel Hammoud <devjamel@gmail.com>
2026-08-01 18:21:15 -0700

    crew sync

--- src/renderer/src/components/MessageReactions.tsx
+++ src/renderer/src/components/MessageReactions.tsx
@@ -123,18 +123,6 @@ export default function MessageReactions({
           className="flex h-7 w-7 shrink-0 items-center justify-center rounded-full text-fg-secondary transition-[transform,background-color,color] duration-200 ease-[cubic-bezier(0.22,1,0.36,1)] hover:scale-105 hover:bg-fg/8 hover:text-fg active:scale-90"
         />
         {deletable && (
-          <Tooltip label="Delete">
-            <button
-              type="button"
-              aria-label="Delete message"
-              onClick={onDelete}
-              className="hidden h-7 w-7 shrink-0 items-center justify-center rounded-full text-danger transition-[transform,background-color] duration-200 ease-[cubic-bezier(0.22,1,0.36,1)] hover:scale-105 hover:bg-danger/10 active:scale-90 shift:flex"
-            >
-              <TrashGlyph className="h-4 w-4" />
-            </button>
-          </Tooltip>
-        )}
-        {deletable && (
           <span className="relative">
             <Tooltip label="More" disabled={actionsOpen}>
               <button
