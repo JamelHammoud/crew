cc18ad619941a0554bef1f5abd70c002e8ca6964
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:24:19 -0700

    crew sync

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -151,7 +151,7 @@ function Session() {
         </main>
         <div className="absolute top-0 inset-x-0 z-40 pointer-events-none">
           <div className="top-bar-container pointer-events-auto bg-ink-900">
-            <TopBar tab={tab} onTab={switchTab} />
+            <TopBar />
           </div>
           {tab !== 'design' && <div className="h-10 bg-gradient-to-b from-ink-900 to-transparent" />}
         </div>
