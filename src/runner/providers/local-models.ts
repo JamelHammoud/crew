0e5bc93cbb6b2bbe47526a59228f61e9ee73b0ab
Jamel Hammoud <devjamel@gmail.com>
2026-08-05 09:33:43 -0700

    crew sync

--- src/runner/providers/local-models.ts
+++ src/runner/providers/local-models.ts
@@ -1,6 +1,7 @@
 import { readdirSync } from 'node:fs'
 import { homedir } from 'node:os'
 import { join } from 'node:path'
+import { openaiUrl } from '../../shared/modelServers'
 import type { LocalRuntime } from './local-serve'
 
 export interface LocalModel {
