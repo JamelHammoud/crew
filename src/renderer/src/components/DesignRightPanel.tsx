f1ea5637c9175113e9dd15241b9368030108bb7c
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:54:13 -0700

    crew sync

--- src/renderer/src/components/DesignRightPanel.tsx
+++ src/renderer/src/components/DesignRightPanel.tsx
@@ -1,4 +1,5 @@
 import { BOARD_CHAT_MARK } from '../design/designKeys'
+import { RIGHT_PANEL_W } from '../design/headerBand'
 import DesignChat from './DesignChat'
 
 export default function DesignRightPanel({ boardId, onClose }: { boardId: string; onClose: () => void }) {
@@ -6,7 +7,8 @@ export default function DesignRightPanel({ boardId, onClose }: { boardId: string
     <aside
       aria-label="Board chat"
       {...{ [BOARD_CHAT_MARK]: '' }}
-      className="w-[340px] shrink-0 flex flex-col min-w-0 min-h-0 overflow-hidden bg-ink-900 border-l border-ink-700"
+      style={{ width: RIGHT_PANEL_W }}
+      className="shrink-0 flex flex-col min-w-0 min-h-0 overflow-hidden bg-ink-900 border-l border-ink-700"
     >
       <DesignChat key={boardId} boardId={boardId} onClose={onClose} />
     </aside>
