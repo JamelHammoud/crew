3324abf7399e2e3ccad7bd0c198b51e7ac6e51ef
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 20:34:26 -0700

    crew sync

--- tests/browser-tabs-probe.test.ts
+++ tests/browser-tabs-probe.test.ts
@@ -81,7 +81,7 @@ const laidOutRow = () => {
   HTMLElement.prototype.getBoundingClientRect = function (this: HTMLElement) {
     if (this.dataset.reorder !== undefined) {
       const at = Array.prototype.indexOf.call(this.parentElement?.children ?? [], this)
-      return box(at * 100, 90)
+      return box(at * 100 - (this.parentElement?.scrollLeft ?? 0), 90)
     }
     return strip(this) ? box(0, VIEW) : box(0, 0)
   }
