6b153dcae4ce1fff2ecb483ffb1de5f80c0be9c9
Jamel Hammoud <devjamel@gmail.com>
2026-08-05 07:58:45 -0700

    crew sync

---
+++ src/renderer/src/components/sidebar/NewPage.tsx
@@ -0,0 +1,16 @@
+import { PlusGlyph } from '../../icons'
+import Tooltip from '../Tooltip'
+
+export default function NewPage({ onClick }: { onClick: () => void }) {
+  return (
+    <Tooltip label="New page">
+      <button
+        onClick={onClick}
+        aria-label="New page"
+        className="app-no-drag h-6 w-6 rounded-lg flex items-center justify-center opacity-0 text-fg/45 transition-[color,background-color,opacity] duration-150 active:scale-[0.95] group-hover:opacity-100 focus-visible:opacity-100 hover:bg-fg/[0.10] hover:text-fg"
+      >
+        <PlusGlyph className="w-4 h-4" />
+      </button>
+    </Tooltip>
+  )
+}
