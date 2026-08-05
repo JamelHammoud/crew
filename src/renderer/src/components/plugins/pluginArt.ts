f5eceff5ff69bfb5ceba4a0895a5a0c7f25e9276
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:52:34 -0700

    crew sync

--- src/renderer/src/components/plugins/pluginArt.ts
+++ src/renderer/src/components/plugins/pluginArt.ts
@@ -1,18 +1,44 @@
+import airtableMark from '../../media/plugins/airtable.png'
+import asanaMark from '../../media/plugins/asana.png'
+import atlassianMark from '../../media/plugins/atlassian.png'
+import canvaMark from '../../media/plugins/canva.png'
+import chromeMark from '../../media/plugins/chrome-devtools.png'
+import cloudflareMark from '../../media/plugins/cloudflare.png'
 import context7Mark from '../../media/plugins/context7.png'
+import deepwikiMark from '../../media/plugins/deepwiki.png'
 import figmaMark from '../../media/plugins/figma.png'
 import githubMark from '../../media/plugins/github.png'
+import intercomMark from '../../media/plugins/intercom.png'
 import linearMark from '../../media/plugins/linear.png'
+import netlifyMark from '../../media/plugins/netlify.png'
 import notionMark from '../../media/plugins/notion.png'
 import playwrightMark from '../../media/plugins/playwright.png'
 import sentryMark from '../../media/plugins/sentry.png'
+import slackMark from '../../media/plugins/slack.png'
+import stripeMark from '../../media/plugins/stripe.png'
+import supabaseMark from '../../media/plugins/supabase.png'
+import vercelMark from '../../media/plugins/vercel.png'
 
 export const PLUGIN_ART: Record<string, string> = {
   figma: figmaMark,
   'figma-desktop': figmaMark,
+  canva: canvaMark,
   github: githubMark,
   sentry: sentryMark,
   context7: context7Mark,
   playwright: playwrightMark,
+  'chrome-devtools': chromeMark,
+  deepwiki: deepwikiMark,
+  vercel: vercelMark,
+  netlify: netlifyMark,
+  cloudflare: cloudflareMark,
+  supabase: supabaseMark,
   linear: linearMark,
-  notion: notionMark
+  notion: notionMark,
+  slack: slackMark,
+  atlassian: atlassianMark,
+  asana: asanaMark,
+  intercom: intercomMark,
+  airtable: airtableMark,
+  stripe: stripeMark
 }
