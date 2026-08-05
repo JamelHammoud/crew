d5a29a4e879dca0be62fb6d87fef9d6707a4695e
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:14:16 -0700

    crew sync

--- src/renderer/src/components/plugins/pluginArt.ts
+++ src/renderer/src/components/plugins/pluginArt.ts
@@ -1,1 +1,20 @@
-export const PLUGIN_ART: Record<string, string> = {}
+import context7Mark from '../../media/plugins/context7.png'
+import figmaMark from '../../media/plugins/figma.png'
+import githubMark from '../../media/plugins/github.png'
+import linearMark from '../../media/plugins/linear.png'
+import notionMark from '../../media/plugins/notion.png'
+import playwrightMark from '../../media/plugins/playwright.png'
+import sentryMark from '../../media/plugins/sentry.png'
+import slackMark from '../../media/plugins/slack.png'
+
+export const PLUGIN_ART: Record<string, string> = {
+  figma: figmaMark,
+  'figma-desktop': figmaMark,
+  github: githubMark,
+  sentry: sentryMark,
+  context7: context7Mark,
+  playwright: playwrightMark,
+  linear: linearMark,
+  notion: notionMark,
+  slack: slackMark
+}
