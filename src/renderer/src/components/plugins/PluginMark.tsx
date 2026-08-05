18c6ac2c9d9df68be90b44756ec2a1bd1e33e1e5
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:10:30 -0700

    crew sync

--- src/renderer/src/components/plugins/PluginMark.tsx
+++ src/renderer/src/components/plugins/PluginMark.tsx
@@ -1,13 +1,19 @@
 import GeneratedField from '../art/GeneratedField'
 import InsetRing from '../InsetRing'
+import { PLUGIN_ART } from './pluginArt'
 
 export default function PluginMark({ seed, box = 40 }: { seed: string; box?: number }) {
+  const art = PLUGIN_ART[seed]
   return (
     <span
       className="relative inline-block align-middle shrink-0 overflow-hidden rounded-[22%]"
       style={{ width: box, height: box }}
     >
-      <GeneratedField seed={seed} box={box} />
+      {art ? (
+        <img src={art} alt="" draggable={false} className="w-full h-full object-cover" />
+      ) : (
+        <GeneratedField seed={seed} box={box} />
+      )}
       <InsetRing className="ring-1 ring-inset ring-fg/5" />
     </span>
   )
