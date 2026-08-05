77fef957d308b760947b60fc9edfa1019c092870
Jamel Hammoud <devjamel@gmail.com>
2026-08-04 21:02:03 -0700

    crew sync

--- src/renderer/src/state/store.ts
+++ src/renderer/src/state/store.ts
@@ -32,6 +32,7 @@ import {
 } from '../../../shared/events'
 import type { GameScore } from '../../../shared/games'
 import { cleanMemoryLine, memoryKey, MEMORY_FULL, MEMORY_LIMIT, type CrewMemory } from '../../../shared/memory'
+import { cleanPlugin, pluginKey, PLUGIN_FULL, PLUGIN_LIMIT, type CrewPlugin } from '../../../shared/plugins'
 import type { CrewTool, ToolAction } from '../../../shared/toolbox'
 import { emptyRoom } from '../../../shared/huddle'
 import { emptyMusic, type MusicPlaylist, type MusicUpload } from '../../../shared/music'
