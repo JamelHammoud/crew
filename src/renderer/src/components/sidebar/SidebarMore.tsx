d319e04636f853b2a9fdf8718c6c308bb8a04569
Jamel Hammoud <devjamel@gmail.com>
2026-08-05 20:07:22 -0700

    crew sync

--- src/renderer/src/components/sidebar/SidebarMore.tsx
+++ src/renderer/src/components/sidebar/SidebarMore.tsx
@@ -74,12 +74,7 @@ export default function SidebarMore({ tab, onTab }: { tab: Tab; onTab: (tab: Tab
                 }}
               />
             ))}
-            <MenuItem
-              icon={<PanelRightGlyph />}
-              label="Browser"
-              active={panel}
-              onClick={openBrowser}
-            />
+            <MenuItem icon={<CompassGlyph />} label="Browser" onClick={openBrowser} />
             <MenuItem icon={<ToolboxGlyph />} label="Toolbox" onClick={openToolbox} />
           </div>
         </Popover>
