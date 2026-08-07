ee2c50cbd32e5ee72516e7f371dcf4424b8e56bc
Jamel Hammoud <devjamel@gmail.com>
2026-08-07 12:43:21 -0700

    crew sync

--- src/renderer/src/components/DocEditor.tsx
+++ src/renderer/src/components/DocEditor.tsx
@@ -138,6 +138,7 @@ export default forwardRef<DocEditorHandle, { text: string; onChange: (markdown: 
           emojiPicker={false}
           sideMenu={false}
           filePanel={false}
+          tableHandles={false}
         >
           <FormattingToolbarController formattingToolbar={DocToolbar} />
           <SuggestionMenuController
