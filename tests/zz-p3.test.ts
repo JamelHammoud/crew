a199843f8a0d35691c3ab54a0da7b8e42fdbea61
Jamel Hammoud <devjamel@gmail.com>
2026-08-11 21:43:33 -0700

    crew sync

---
+++ tests/zz-p3.test.ts
@@ -0,0 +1,3 @@
+import { createElement, type ReactNode } from 'react'
+declare const R: (p: Record<string, unknown>) => ReactNode
+export const x = Array.from({ length: 3 }, (_, i) => createElement(R, { key: i, targetId: 't', authorId: 'a', authorName: 'A' }))
