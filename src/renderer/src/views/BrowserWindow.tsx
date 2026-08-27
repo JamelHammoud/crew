84d6f27f43a9429be4f8e8c42a54df2fa3010c0e
Jamel Hammoud <devjamel@gmail.com>
2026-08-26 21:10:47 -0700

    crew sync

--- src/renderer/src/views/BrowserWindow.tsx
+++ src/renderer/src/views/BrowserWindow.tsx
@@ -1,7 +1,14 @@
 import BrowserPanel from '../components/BrowserPanel'
+import { browserTabLabel } from '../components/BrowserTabMark'
 import Toaster from '../components/Toaster'
+import { useBrowser } from '../state/browser'
+import { useWindowName } from '../state/windowName'
 
 export default function BrowserWindow() {
+  const tab = useBrowser(s => s.tabs.find(one => one.id === s.activeTabId))
+
+  useWindowName(tab ? browserTabLabel(tab) : '')
+
   return (
     <div data-browser-window className="h-full relative bg-ink-900">
       <BrowserPanel standalone />
