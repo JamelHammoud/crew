4b10a99f919008307e80d2e1c83fb7a8b7c6c828
Jamel Hammoud <devjamel@gmail.com>
2026-08-29 16:33:17 -0700

    crew sync

--- src/renderer/src/components/useSidebarWindowGlass.ts
+++ src/renderer/src/components/useSidebarWindowGlass.ts
@@ -1,10 +1,10 @@
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 import { usePrefs } from '../state/prefs'
 
 export default function useSidebarWindowGlass(): boolean {
   const glass = usePrefs().glassSidebar
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const root = document.getElementById('root')
     root?.classList.toggle('sidebar-window-glass', glass)
     return () => root?.classList.remove('sidebar-window-glass')
