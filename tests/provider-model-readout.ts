5719578d0c2120fff761c47054e9c4cadf7a7ad6
Jamel Hammoud <devjamel@gmail.com>
2026-09-06 10:59:10 -0700

    crew sync

--- tests/provider-model-readout.ts
+++
@@ -1,9 +0,0 @@
-import { builtinProviders } from '../src/runner/providers/detect'
-
-for (const provider of builtinProviders) {
-  const installed = await provider.detect()
-  const model = provider.fields().find(field => field.key === 'model')
-  console.log(
-    JSON.stringify({ provider: provider.name, installed, models: model?.options?.map(option => option.value) ?? [] })
-  )
-}
